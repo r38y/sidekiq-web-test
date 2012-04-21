06745fb6771523ee8edede027aeee9ed51461585
Randy Schmidt <randy@forge38.com>
2012-04-21 18:12:28 -0400

    yeah

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SidekiqTest::Application.config.secret_token = '78e5ed6f673805c1943644ce1dd5efe71eeb5b7abd60e786f9ca7df578ea57110afd40f0918f0415df65a6522e6d648ac19933888848e3f17dc75239973cb46f'
